c74824a3c44e203744147fae097946227a3f8f48
Julie Rich <julierich1211@yahoo.com>
2014-10-23 14:57:31 -0500

    update

--- js/app.js
+++ js/app.js
@@ -1,18 +1,42 @@
-
 window.onload = app;
 
 // runs when the DOM is loaded
-function app(){
+function app() {
 
-    // load some scripts (uses promises :D)
-    loader.load(
-        {url: "./bower_components/jquery/dist/jquery.min.js"},
-        {url: "./bower_components/lodash/dist/lodash.min.js"},
-        {url: "./bower_components/pathjs/path.min.js"}
-    ).then(function(){
-        _.templateSettings.interpolate = /{([\s\S]+?)}/g;
+        // load some scripts (uses promises :D)
+        loader.load({
+            url: "./bower_components/jquery/dist/jquery.min.js"
+        }, {
+            url: "./bower_components/lodash/dist/lodash.min.js"
+        }, {
+            url: "./bower_components/pathjs/path.min.js"
+        }).then(function() {
+            _.templateSettings.interpolate = /{([\s\S]+?)}/g;
 
-        // start app?
-    })
+            // start app?
+        })
+var flickr = new Flickr({
+  api_key: "1234ABCD1234ABCD1234ABCD1234ABCD"
+});
+    } 
+//upload to flickr
+   //Flickr.authenticate(FlickrOptions, function(error, flickr) {
+  //var uploadOptions = {
+    //photos: [{
+      //title: "test",
+      //tags: ["happy","fox"],
+     // photo: __dirname + "/test.jpg"
+   // },{
+     // title: "test2",
+      //tags: ["secondary", "test", "image"],
+      //photo: __dirname + "/test.jpg"
+   // }]
+ // };
 
-}
+  //Flickr.upload(uploadOptions, FlickrOptions, function(err, result) {
+    //if(err) {
+      //return console.error(error);
+    //}
+    //console.log("photos uploaded", result);
+ // });
+//});
