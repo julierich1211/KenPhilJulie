f773708dba2213fce26f5cdc187357f1afa185d8
warnerp18 <warnerp18@gmail.com>
2014-10-24 14:46:43 -0500

    beerMe

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ window.onload = app;
 
 // runs when the DOM is loaded
 function app() {
+  "use strict"
 
         // load some scripts (uses promises :D)
         loader.load({
@@ -17,6 +18,26 @@ function app() {
               api_key: "c6e086abdb374e9c5b9de44506e25ecb"
             }
             // start app?
-          var beer = new Beerme (options); 
+          var beer = new beerMe (options); 
           })
 }
+
+function beerMe(options) {
+  "use strict"
+  if (!options.api_key) {
+    throw new Error ("Yo dawg, I heard you like APIs. Y U NO APIKEY?!?!?!");
+  }
+  this.brewerydb_url = "http: //api.brewerydb.com/";
+  this.version = "v2/";
+  this.api_key = options.api_key;
+  this.complete_api_url = this.brewerydb_url + this.version;
+
+  this.init();
+
+  console.log(beerMe);
+}
+
+beerMe.prototype.init = function() {
+  "use strict";
+
+  };
