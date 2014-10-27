2deeaa7f030df9c6a419f0f07ce8b3e016d07c1e
warnerp18 <warnerp18@gmail.com>
2014-10-27 15:00:12 -0500

    not working

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 window.onload = app;
 
 // runs when the DOM is loaded
+
 function app() {
-    "use strict";
 
     // load some scripts (uses promises :D)
     loader.load({
@@ -14,104 +14,108 @@ function app() {
     }).then(function() {
         _.templateSettings.interpolate = /{([\s\S]+?)}/g;
 
-
         var options = {
-                api_key: "c6e086abdb374e9c5b9de44506e25ecb"
-            };
-            // start app?
-        var beer = new BeerMe(options);
+            api_key: "c6e086abdb374e9c5b9de44506e25ecb"
+        };
+        // start app?
+        var client = new BeerClient(options);
     });
+
 }
 
-function beerMe(options) {
-    "use strict";
+function BeerClient(options) {
     if (!options.api_key) {
-        throw new Error("Yo dawg, I heard you like APIs. Y U NO APIKEY?!?!?!");
-    }
-    this.brewerydb_url = "http: //api.brewerydb.com/";
-    this.version = "v2/";
+        throw new Error("Yo dawg, I heard you like APIs. Y U NO APIKEY!?!?");
+    } else { return
+    this.bdb_url = "http://api.brewerydb.com/v2";
     this.api_key = options.api_key;
-    this.complete_api_url = this.brewerydb_url + this.version;
+    this.complete_api_url = this.bdb_url;
 
-    this.init();
-
-    console.log(new BeerMe);
+    // derp.
+    this.init();}
 }
 
-beerMe.prototype.pullAllStyles = function() {
-    "use strict";
+BeerClient.prototype.pullAllActiveListings = function() {
     return $.getJSON(
-            this.complete_api_url + "styles/?key=" + this.api_key + "&callback=?")
+
+       this.complete_api_url + "search/style" + "?key=" + this.api_key + "&q=ipa";
+    )
         .then(function(data) {
+            console.log(data);
             return data;
-
         });
-
 };
-beerMe.prototype.pullSingleCategoryID = function(id) {
-    return $.getJSON(this.complete_api_url + "styles/" + id + ".js?api_key=" + this.api_key + "&callback=?").then(function(data) {
+
+BeerClient.prototype.pullSingleListing = function(id) {
+    return $.getJSON(this.complete_api_url + "search/style" + "?key=" + this.api_key + "&q=ipa").then(function(data) {
         return data;
     });
-
 };
-beerMe.prototype.loadTemplate = function(name) {
+
+BeerClient.prototype.loadTemplate = function(name) {
     if (!this.templates) {
         this.templates = {};
     }
+
     var self = this;
+
     if (this.templates[name]) {
         var promise = $.Deferred();
         promise.resolve(this.templates[name]);
         return promise;
     } else {
         return $.get('./templates/' + name + '.html').then(function(data) {
-            self.templates[name] = data;
+            self.templates[name] = data; // <-- cache it for any subsequent requests to this template
             return data;
-
-
         });
     }
 };
-beerMe.prototype.drawStyle = function(templateString, data) {
+
+BeerClient.prototype.drawListings = function(templateString, data) {
     var grid = document.querySelector("#formOne");
-    var bigHTMLString = data.results.map(function(style) {
-        return_.template(templateString, style);
+
+    var bigHtmlString = data.results.map(function(formBegin) {
+        return _.template(templateString, formBegin);
     }).join('');
-    grid.innerHTML = bigHTMLString;
+
+    grid.innerHTML = bigHtmlString;
 };
-beerMe.prototype.drawSingleCategoryID = function(template, data) {
-    var style = data.results[0];
+
+BeerClient.prototype.drawSingleListing = function(template, data) {
+    var listing = data.results[0];
     var grid = document.querySelector("#formOne");
-    var bigHTMLString = _.template(template, style);
+    var bigHtmlString = _.template(template, formBegin);
 
-    grid.innerHTML = bigHTMLString;
+    grid.innerHTML = bigHtmlString;
 };
 
-beerMe.prototype.init = function() {
-    "use strict";
+BeerClient.prototype.setupRouting = function(){
     var self = this;
 
     Path.map("#/").to(function() {
-        $.when(
-            self.loadTemplate("style"),
-            self.pullAllStyles()
-        ).then(function() {
-            self.drawStyle(arguments[0], arguments[1]);
-            console.dir(self);
-        });
+        self.drawStyle(self.formBiginHtml, self.latestData);
     });
-    Path.map("#/message/:anymessage").to(function() {
-        alert(this.params.anymessage);
 
+    Path.map("#/message/:anymessage").to(function(){
+        alert(this.params.anymessage);
     });
-    Path.map("#/style/:id").to(function() {
-        $.when(
-            self.loadTemplate("drawSingleCategoryID"),
-            self.pullSingleCategoryID(this.params.id)
-        ).then(function() {
-            self.drawSingleCategoryID(arguments[0], arguments[1]);
-        });
+
+    Path.map("#/categoryID/:id").to(function() {
+        self.drawSingleCategoryID(this.params.id);
     });
+
+    // set the default hash
     Path.root("#/");
-    Path.listen();
+};
+
+BeerClient.prototype.init = function() {
+    var self = this;
+    // start doing shit...
+    $.when(
+        this.pullAllActiveListings(),
+        this.pullSingleListing(),
+        this.loadTemplate('formBegin')
+    ).then(function(formBegin, formBeginHtml) {
+        self.pullAllActiveListings(formBeginHtml, formBegin);
+    });
 };
--- js/app2.js
+++ js/app2.js
@@ -1,8 +1,9 @@
-//_.templateSettings.interpolate = /{([\s\S]+?)}/g;
+window.onload = app;
+
 
 function pullDetail(beerName) {
     this.beerName = beerName;
-    this.detail();
+    //this.detail();
 }
 pullDetail.prototype.getInfo = function() {
     return $.get('https://api.openbeerdatabase.com/v1/beers.json' + this.beerName).then(function(data) {
@@ -13,8 +14,9 @@ pullDetail.prototype.getInfoBeer = function() {
     return $.get('https://api.openbeerdatabase.com/v1/beers.json' + this.beerName + '/openBeer').then(function(data) {
         return data;
     });
-    console.log(data);
+   
 };
+
 pullDetail.prototype.loadTemplate = function(templateName) {
     return $.get('templates/' + templateName + '.html').then(function(hstring) {
         return hstring;
@@ -25,25 +27,46 @@ pullDetail.prototype.placeDetail = function(beerHTML, beer) {
     beerName.joined = ["Joined On", d.toDateString()].join("");
     document.querySelector('').innerHTML = _.template(beerHTML, beer);
 };
-pullDetail.prototype.placeBeerData = function(openBeerHTML, openBeer) {
+pullDetail.prototype.placeBeerData = function(id) {
     document.querySelector('#formOne').innerHTML = openBeer.map(function(openBeer){
         return _.template(openBeerHTML, openBeer);
     }).join('');
 };
+
+pullDetail.prototype.setupRouting = function(){
+    var self = this;
+
+    Path.map("#/").to(function() {
+        self.placeDetail(self.formBiginHtml, self.latestData);
+    });
+
+    Path.map("#/message/:anymessage").to(function(){
+        alert(this.params.anymessage);
+    })
+
+    Path.map("#/listing/:id").to(function() {
+        self.placeBeerData(this.params.id);
+    });
+
+    // set the default hash
+    Path.root("#/");
+}
+
 pullDetail.prototype.detail = function() {
     var own = this;
+    this.setupRouting();
 
     $.when(
         this.getInfo(),
         this.getInfoBeer(),
         this.loadTemplate('formBegin')
         //this.loadTemplate('openBeer')
-    ).then(function(formBegin, formBeginHTML) {
+    ).then(function(formBeginHTML, formBegin) {
         own.placeDetail(formBeginHTML, formBegin);
         //own.placeRepoData(openBeerHTML, openBeer);
     });
 }
-window.onload = app;
+
 
 function app() {
     var subject = new pullDetail('beerName');
