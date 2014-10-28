fee7317bb7a56c1b5f863a434ef897d8dc226054
Julie Rich <julierich1211@yahoo.com>
2014-10-28 10:57:54 -0500

    newestattempt

--- js/app.js
+++ js/app.js
@@ -23,18 +23,6 @@ function app() {
 
 }
 
-<<<<<<< HEAD
-=======
-function BeerClient(options) {
-    if (!options.api_key) {
-        throw new Error("Yo dawg, I heard you like APIs. Y U NO APIKEY!?!?");
-
-    } else {
->>>>>>> 022d65cb12fd2fe48888f5dedd295b879f43a3b0
-
-
-
-<<<<<<< HEAD
 function BeerClient(options) {
     var brewerydb = $.getJSON("./js/brewerydb.json", function() {
             console.log("success");
@@ -48,40 +36,19 @@ function BeerClient(options) {
         .always(function() {
             console.log("complete");
         });
-=======
-        // derp.
-        this.init();
-    }
-
->>>>>>> 022d65cb12fd2fe48888f5dedd295b879f43a3b0
-}
-
-function pullAllActiveListings(callback) {
-    $.getJSON("./js/brewerydb.json",
-        function(data) {
-            var allBeer = [];
-            $.each(data.items,
-                function(item) {
-                    allBeer.push(item);
-                });
-            callback(allBeer);
-            // callback(data.items); should also work
-        });
 }
 
 BeerClient.prototype.pullAllActiveListings = function() {
-    return $.getJSON('brewerydb.json').then(function(data) {
+    return $.getJSON(brewerydb).then(function(data) {
         console.log(data);
         return data;
     });
 };
 
 BeerClient.prototype.pullSingleListing = function(id) {
-    return $.getJSON('brewerydb.json').then(function(data) {
-        console.log(data);
+    return $.getJSON(brewerydb).then(function(data) {
         return data;
     });
-
 };
 
 BeerClient.prototype.loadTemplate = function(name) {
@@ -101,69 +68,34 @@ BeerClient.prototype.loadTemplate = function(name) {
             return data;
         });
     }
-    BeerClient.prototype.init = function() {
-        var self = this;
-        // start doing shit...
-        $.when(
-            this.pullAllActiveListings(),
-            this.pullSingleListing(),
-            this.loadTemplate('formBegin')
-        ).then(function(formBegin, formBeginHtml) {
-            self.pullAllActiveListings(formBeginHtml, formBegin);
-        });
-    };
+};
 
-    BeerClient.prototype.drawListings = function(templateString, data) {
-        var grid = document.querySelector("#formOne");
+BeerClient.prototype.drawListings = function(templateString, data) {
+    var grid = document.querySelector("#formOne");
 
-        var bigHtmlString = data.results.map(function(formBegin) {
-            return _.template(templateString, formBegin);
-        }).join('');
+    var bigHtmlString = data.results.map(function(formBegin) {
+        return _.template(templateString, formBegin);
+    }).join('');
 
-        grid.innerHTML = bigHtmlString;
-    };
+    grid.innerHTML = bigHtmlString;
+};
 
-    BeerClient.prototype.drawSingleListing = function(template, data) {
-        var listing = data.results[0];
-        var grid = document.querySelector("#formOne");
-        var bigHtmlString = _.template(template, formBegin);
+BeerClient.prototype.drawSingleListing = function(template, data) {
+    var listing = data.results[0];
+    var grid = document.querySelector("#formOne");
+    var bigHtmlString = _.template(template, formBegin);
 
-        grid.innerHTML = bigHtmlString;
-    };
-
-<<<<<<< HEAD
-    // BeerClient.prototype.init = function() {
-    //   var self = this;
-=======
-/*
-BeerClient.prototype.setupRouting = function(){
-    var self = this;
-
-    Path.map("#/").to(function() {
-        self.drawStyle(self.formBiginHtml, self.latestData);
-    });
-
-    Path.map("#/message/:anymessage").to(function(){
-        alert(this.params.anymessage);
-    });
-
-    Path.map("#/categoryID/:id").to(function() {
-        self.drawSingleCategoryID(this.params.id);
-    });
-
-    // set the default hash
-    Path.root("#/");
-*/
+    grid.innerHTML = bigHtmlString;
+};
 
 BeerClient.prototype.init = function() {
     var self = this;
->>>>>>> 022d65cb12fd2fe48888f5dedd295b879f43a3b0
     // start doing shit...
-    // $.when(
-    //   this.pullAllActiveListings(),
-    // this.pullSingleListing(),
-    // this.loadTemplate('formBegin')
-    //).then(function(formBegin, formBeginHtml) {
-    //  self.pullAllActiveListings(formBeginHtml, formBegin);
-    //});
+    $.when(
+        this.pullAllActiveListings(),
+        this.pullSingleListing(),
+        this.loadTemplate('formBegin')
+    ).then(function(formBegin, formBeginHtml) {
+        self.pullAllActiveListings(formBeginHtml, formBegin);
+    });
 };
