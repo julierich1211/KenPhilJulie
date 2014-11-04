724efff2db8f709e69aabe6310ba9079636ef0fd
Julie Rich <julierich1211@yahoo.com>
2014-11-03 21:25:16 -0600

    working

--- js/kapp.js
+++ js/kapp.js
@@ -1,133 +1,175 @@
-window.onload = app;
+ window.onload = app;
 
-// runs when the DOM is loaded
+ // runs when the DOM is loaded
 
+ // Global variables
+ ident = [];
+ styles = [];
 
+ function counter() {
+     // debugger;
+     var getMeNumber = localStorage.getItem("visits");
+     if (getMeNumber == null || isNaN(getMeNumber)) {
+         getMeNumber = 0;
+     } else {
+         getMeNumber = parseInt(getMeNumber);
+     }
+     getMeNumber++;
+     localStorage.setItem("visits", getMeNumber.toString())
+     document.querySelector("#visited").innerHTML = localStorage.getItem("visits");
+ }
 
-
-function app() {
-
-    // load some scripts (uses promises :D)
-    loader.load({
-        url: "./bower_components/jquery/dist/jquery.min.js"
-    }, {
-        url: "./bower_components/lodash/dist/lodash.min.js"
-    }, {
-        url: "./bower_components/pathjs/path.min.js"
-    }).then(function() {
-        _.templateSettings.interpolate = /{([\s\S]+?)}/g;
-
-        var options = {
-            app_key: "b5ba65d32dc965e46b6a000b8c532a56",
-            app_id: "db9d8943"
-        };
-        // start app?
-        var client = new YummlyClient(options);
-    });
-
-}
-
-function YummlyClient(options) {
-    if (!options.app_key) {
-        throw new Error("Y U NO KEY?!?!?");
-    }
-    if (!options.app_id) {
-        throw new Error("Y U NO ID?!?!?");
-    }
-    this.yummly_url = "http://api.yummly.com/v1/api/recipes?_app_id=";
-    this.api_key = options.app_key;
-    this.api_id = options.app_id;
-    this.cuisine = "&allowedCuisine[]=cuisine^cuisine-";
-    this.course = "&allowedCourse[]=course^course-";
-    this.pictures = "&requirePictures=true";
-    this.complete_api_url = this.yummly_url + this.app_id + "&_app_key=" + this.app_key;
-
-    this.setupRouting();
-    console.log("set up routing works");
-}
-
-// from Shawn/Adam attempting to undertsand the 
-// use & creation of an input object for forms
-YummlyClient.prototype.createInputObject = function() {
-    "use strict";
-    var input = {};
-    $(':input').each(function() {
-        input[this.name] = this.value;
-    });
-
-    return input;
-};
-
-YummlyClient.prototype.takeRecipes = function() {
-
-    var input = this.createInputObject();
-
-    return $.getJSON(
-        this.complete_api_url + this.course + input.course + this.cuisine + input.cuisine
-    ).then(function(data) {
-
-        return data.matches;
-    });
-};
-
-/*YummlyClient.prototype.takeSingleRecipe = function(id) {
-    return $.getJSON(
-        this.single
-        ).then(function(data) {
-        return data.matches;
-    });
-};*/
-
-YummlyClient.prototype.loadTemplate = function(name) {
-    return $.get('./templates/' + name + '.html').then(function(data) {
-        return data;
-    });
-};
-
-YummlyClient.prototype.giveRecipes = function(templateString, data) {
-    var grid = document.querySelector("#lefty");
-
-    var bigHtmlString = data.results.map(function(listing) {
-        return _.template(templateString, listing);
-    }).join('');
-
-    grid.innerHTML = bigHtmlString;
-};
+ counter();
 
 
-/*YummlyClient.prototype.giveSingleRecipe = function(template, data) {
-    var listing = data[0];
-    var grid = document.querySelector("#lefty");
-    var bigHtmlString = _.template(template, listing);
+ function app() {
 
-    grid.innerHTML = bigHtmlString;
-}*/
+         // load some scripts (uses promises :D)
+         loader.load({
 
-YummlyClient.prototype.setupRouting = function() {
-    var self = this;
+             url: "./bower_components/jquery/dist/jquery.min.js"
+         }, {
+             url: "./bower_components/lodash/dist/lodash.min.js"
+         }, {
+             url: "./bower_components/pathjs/path.min.js"
+         }, {
+             url: "./bower_components/bower_components/foundation/js/foundation.js"
+         }).then(function() {
+             _.templateSettings.interpolate = /{([\s\S]+?)}/g;
 
-    Path.map("#/results").to(function() {
-        $.when(
-            self.takeRecipes(),
-            self.loadTemplate("recipeResults")
-        ).then(function(recipedata, recipeResultsHtml) {
-            self.giveRecipes(recipedata, recipeResultsHtml);
+             var options = {
+                 app_key: "b5ba65d32dc965e46b6a000b8c532a56",
+                 app_id: "db9d8943"
+             };
+             // start app?
+             var client = new YummlyClient(options);
+         });
 
-            console.dir(self);
-        });
-    });
+     }
+     // http://api.yummly.com/v1/api/recipes?_app_id=app-id
+     // &_app_key=app-key&your _search_parameters
+ function YummlyClient(options) {
+     "use strict";
+     if (!options.app_key) {
+         throw new Error("Wrong APP_KEY?!?!?");
+     }
+     if (!options.app_id) {
+         throw new Error("Wrong APP_ID?!?!?");
+     }
+     this.yummly_url = "http://api.yummly.com/v1/api/recipes?_app_id=";
+     this.ingredient = "&allowedIngredient[]=";
+     this.cuisine = "&allowedCuisine[]=cuisine^cuisine-";
+     this.course = "&allowedCourse[]=course^course-";
+     //this.pictures = "&requirePictures=true";
+     this.app_id = options.app_id;
+     this.app_key = options.app_key;
+     this.complete_api_url = this.yummly_url + this.app_id + "&_app_key=" + this.app_key;
 
 
-    /*Path.map("#/left/:id").to(function() {
-        $.when(
-            self.loadTemplate("right"),
-            self.takeSingleRecipe(this.params.id)
-        ).then(function() {
-            self.giveSingleRecipe(arguments[0], arguments[1]);
-        })
-    });*/
+     console.log(this.complete_api_url);
+     console.log("set up routing works");
+     this.setupRouting();
 
-    // set the default hash to draw all listings
-    Path.root("#/");
-    Path.listen();
-};
+
+ }
+
+ // from Shawn/Adam attempting to undertsand the 
+ // use & creation of an input object for forms
+ YummlyClient.prototype.createInputObject = function() {
+     "use strict";
+     var input = {};
+     $(':input').each(function() {
+         input[this.name] = this.value;
+     });
+     console.dir(input);
+     return input;
+
+
+
+ };
+
+ YummlyClient.prototype.takeRecipes = function() {
+
+     var input = this.createInputObject();
+
+     return $.getJSON(
+         this.complete_api_url + this.course + this.course + input.course + this.course + this.cuisine + input.cuisine
+     ).then(function(data) {
+         console.log(data);
+         console.log("takeRecipes Working");
+         return data.matches;
+
+     });
+ };
+
+
+ YummlyClient.prototype.loadTemplate = function(template) {
+     return $.get('./templates/' + template + '.html').then(function(htmlString) {
+         return htmlString;
+     });
+ };
+ YummlyClient.prototype.placeRecipe = function(data, html) {
+     "use strict";
+     // debugger;
+     console.log(data);
+     console.log(html);
+     document.querySelector('#recipeResults').innerHTML =
+         data.map(function(element, index) {
+             ident.push(element.id);
+              return _.template(html, $.extend({}, element));
+        }).join("");
+ };
+
+ //YummlyClient.prototype.giveRecipes = function(data, html) {
+ //  var grid = document.querySelector('#lefty');
+
+ //var bigHtmlString = data.matches.map(function(html) {
+ //  return _.template(data, html);
+ //}).join('');
+
+ //grid.innerHTML = bigHtmlString;
+ //};
+
+
+ /*YummlyClient.prototype.giveSingleRecipe = function(template, data) {
+     var listing = data[0];
+     var grid = document.querySelector("#lefty");
+     var bigHtmlString = _.template(template, listing);
+
+     grid.innerHTML = bigHtmlString;
+ }*/
+
+ YummlyClient.prototype.setupRouting = function() {
+     "use strict";
+     var self = this;
+
+
+     Path.map("#/results").to(function() {
+         $.when(
+             self.takeRecipes(),
+             self.loadTemplate('recipeResults')
+         ).then(function(data, recipeResultsHtml) {
+               console.log(data)
+             self.placeRecipe(data, recipeResultsHtml);
+             // self.giveRecipes(data, recipeResultsHtml);
+             console.log(ident);
+
+         });
+     });
+
+     $("#refresh").click(function() {
+         $.when(
+             self.takeRecipes(),
+             self.loadTemplate('recipeResults')
+         ).then(function(data, recipeResultsHtml) {
+             self.placeRecipe(data, recipeResultsHtml);
+             console.log(ident);
+
+         });
+     });
+
+
+     // set the default hash to draw all listings
+     Path.root("#/");
+     Path.listen();
+ };
